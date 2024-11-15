c870f8c73e7392bbe002247933c5b630603a4149
Nyan Lin Aung <150507886+blaze-nyan@users.noreply.github.com>
2024-11-15 09:56:30 +0700

    add: added popup cancel button on each ingredients

--- src/App.tsx
+++ src/App.tsx
@@ -25,18 +25,31 @@ const App = () => {
       <Route
         path="/"
         element={
-          <div className="bg-gray-900 h-screen text-center p-5 text-white">
+          <div className="bg-gray-900 min-h-screen text-center p-5 text-white">
             <h3 className="text-yellow-400 text-3xl">Make Custom Pizza</h3>
             <h4 className="text-2xl underline mb-4">Choose Addon</h4>
             <ChooseVegetable handleAddOn={handleAddOn} addOn={addOn} />
             <div className="w-full h-1 bg-white mt-2"></div>
-            <ul className=" w-1/3 m-auto rounded-md mt-10 flex flex-row flex-wrap gap-2">
-              {addOn.map((item, index) => (
-                <li key={index} className="bg-black rounded-lg p-1">
-                  {item}
-                </li>
-              ))}
-            </ul>
+            <div className="w-full h-72">
+              <ul className=" w-2/3 m-auto rounded-md mt-10 flex items-start justify-start flex-row flex-wrap gap-2">
+                {addOn.map((item, index) => (
+                  <li
+                    key={index}
+                    className="bg-black rounded-lg p-1 relative cursor-pointer hover:bg-zinc-950 group"
+                  >
+                    {item}
+                    <button
+                      onClick={() =>
+                        setAddOn((prev) => prev.filter((i) => i !== item))
+                      }
+                      className="absolute bg-red-500 rounded-full flex items-center justify-center size-4 top-[-0.5rem] right-[-0.3rem] text-xs opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+                    >
+                      x
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </div>
             <button
               onClick={handleClick}
               className="text-white bg-red-500 p-4 py-2 rounded-md"
